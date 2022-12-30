d17996479ec37a166ddca543398eacd678b27514
toothpasteVR <90981445+toothpasteVR@users.noreply.github.com>
2022-12-30 14:05:26 -0600

    Add files via upload

---
+++ Scripts/slime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class slime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("FallDudesMap1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
